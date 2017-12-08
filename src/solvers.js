e08175ec5880533ea65760d31a4557d4ddf054f8
evthriat <evantatkinson@gmail.com>
2017-12-07 16:00:17 -0800

    fixed solution for N rooks

--- src/solvers.js
+++ src/solvers.js
@@ -15,60 +15,53 @@
 
 
 
-window.findNRooksSolution = function(n, index, row, solution, count) {
-  solution = solution || new Board({n: n});
-  count = count || 0;
-  index = index || 0;
-  row = row || 0;
+window.findNRooksSolution = function(n, index, row, solution) {
+  solution = solution || new Board({n: n});           // the board we are testing
+  //count = count || 0;                                 //number of rooks on board (not used yet)
+  index = index || 0;                                 //index we are modifying and checking
+  row = row || 0;                                     //row we are modifying and checking
   //console.log('solution:', solution)
-
-  solution.togglePiece(row, index);               //toggle rock on row and index
-  if (solution.hasAnyRooksConflicts() === true) { //if toggled rook creates a conflict
-    solution.togglePiece(row, index);               //toggle rook off row and index
+  //if(row === 0) then index === rowOneIndex
+  solution.togglePiece(row, index);                   //toggle rock on row and index
+  if (solution.hasAnyRooksConflicts() === true) {     //if toggled rook creates a conflict
+    solution.togglePiece(row, index);                 //toggle rook off row and index
     //console.log(row, index)
-    index++;                                      //move to next index
+    index++;                                          //move to next index
     if (index >= n) {
-      index = index - n;                          //if index is larger than board, start back at 0
+      index = index - n;                              //if index is larger than board, start back at 0
     }
-    return findNRooksSolution(n, index, row, solution, count);  //call recursively on same row, next index
+    return findNRooksSolution(n, index, row, solution);  //call recursively on same row, next index
   }
   
-  if (row === n - 1) {                                //if board is full return solution
-    return solution;
+  if (row === n - 1) {
+
+    var solutionArr = [];
+    for (var i = 0; i < n; i++) {
+      solutionArr.push(solution.attributes[i]);
+    }                          //if board is full return solution
+    console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solutionArr));
+    return solutionArr;
   }
 
-  if (index > n) {                                //if index is larger than board, start back at 0;
+  if (index > n) {                                    //if index is larger than board, start back at 0;
     index = index - n;
   }
-  row++;                                          //no conflicts, so move to the next row
+  row++;                                              //no conflicts, so move to the next row
   // if (row > n) {
   //   row = row - n;
   // }
-  return findNRooksSolution(n, 0, row, solution, count);  //call recursively on index 0, next row
-  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
-  return solution;
+  
+  return findNRooksSolution(n, 0, row, solution);  //call recursively on index 0, next row
+
 };
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-
-  var solutionCount = 0; //fixme
-  // create for loop to increment over index
-  //for (var r = 0; r < n; r++) {
-    // create another loop for row
-  for (var i = 0; i < n; i++) {
-      //call findNrooks rooks solutions on     
-      // if solution found increment solutionCount
-    if (i === 1 && n === 4) {
-      debugger;
-    }
-      //console.log("Solution: ", findNRooksSolution(n, i, r));
-    if (findNRooksSolution(n, i, 0)) {
-      solutionCount++;
-      console.log('solutionCount: ', solutionCount);
-    }
-    //}
-  }
+  var solutionCount = 0;
+  var testBoard = new Board({n: n});
+  //build board to test
+  //index we're modifying and checking
+  //        
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
 };
